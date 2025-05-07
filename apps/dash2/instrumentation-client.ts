0edbb4b53c8816c0d928479be3e67999b5022993
Hyteq <DerpsDer.es@gmail.com>
2025-05-07 20:07:57 +0300

    fix: reenable sentry, add type fixes

--- apps/dash2/instrumentation-client.ts
+++ apps/dash2/instrumentation-client.ts
@@ -1,28 +1,28 @@
-// // This file configures the initialization of Sentry on the client.
-// // The added config here will be used whenever a users loads a page in their browser.
-// // https://docs.sentry.io/platforms/javascript/guides/nextjs/
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-// import * as Sentry from "@sentry/nextjs";
+import * as Sentry from "@sentry/nextjs";
 
-// Sentry.init({
-//   dsn: "https://8f8c8908f1e96c794be2feff63c79427@o4508943027011584.ingest.de.sentry.io/4509073500012624",
+Sentry.init({
+  dsn: "https://8f8c8908f1e96c794be2feff63c79427@o4508943027011584.ingest.de.sentry.io/4509073500012624",
 
-//   // Add optional integrations for additional features
-//   integrations: [
-//     Sentry.replayIntegration(),
-//   ],
+  // Add optional integrations for additional features
+  integrations: [
+    Sentry.replayIntegration(),
+  ],
 
-//   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-//   tracesSampleRate: 1,
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
 
-//   // Define how likely Replay events are sampled.
-//   // This sets the sample rate to be 10%. You may want this to be 100% while
-//   // in development and sample at a lower rate in production
-//   replaysSessionSampleRate: 0.1,
+  // Define how likely Replay events are sampled.
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
 
-//   // Define how likely Replay events are sampled when an error occurs.
-//   replaysOnErrorSampleRate: 1.0,
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
 
-//   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-//   debug: false,
-// });
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
--- apps/dash2/next.config.ts
+++ apps/dash2/next.config.ts
@@ -8,32 +8,32 @@ const nextConfig: NextConfig = {
 },
 };
 
-export default nextConfig;
+// export default nextConfig;
 
-// export default withSentryConfig(nextConfig, {
-// // For all available options, see:
-// // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+export default withSentryConfig(nextConfig, {
+// For all available options, see:
+// https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-// org: "databuddy",
-// project: "javascript-nextjs",
+org: "databuddy",
+project: "javascript-nextjs",
 
-// // Only print logs for uploading source maps in CI
-// silent: !process.env.CI,
-// telemetry: false,
+// Only print logs for uploading source maps in CI
+silent: !process.env.CI,
+telemetry: false,
 
-// // For all available options, see:
-// // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+// For all available options, see:
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-// // Upload a larger set of source maps for prettier stack traces (increases build time)
-// widenClientFileUpload: true,
+// Upload a larger set of source maps for prettier stack traces (increases build time)
+widenClientFileUpload: true,
 
-// // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// // This can increase your server load as well as your hosting bill.
-// // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// // side errors will fail.
-// tunnelRoute: "/monitoring",
+// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+// This can increase your server load as well as your hosting bill.
+// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+// side errors will fail.
+tunnelRoute: "/monitoring",
 
-// // Automatically tree-shake Sentry logger statements to reduce bundle size
-// disableLogger: true,
+// Automatically tree-shake Sentry logger statements to reduce bundle size
+disableLogger: true,
 
-// });
+});
