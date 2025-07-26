9b7b8d57725b081525adebebbbe9972525e973c8
Hyteq <DerpsDer.es@gmail.com>
2025-07-26 13:56:49 +0300

    pino logger

--- apps/basket/src/lib/logger.ts
+++ apps/basket/src/lib/logger.ts
@@ -2,13 +2,6 @@ import pino from 'pino';
 
 const logger = pino({
 	level: 'info',
-	transport: {
-		target: '@logtail/pino',
-		options: {
-			sourceToken: process.env.LOGTAIL_SOURCE_TOKEN,
-			options: { endpoint: 'https://s1447431.eu-nbg-2.betterstackdata.com' },
-		},
-	},
 });
 
 export { logger };
