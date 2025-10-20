65617e8a3d65e4817c494c155c647163dcd95695
iza <59828082+izadoesdev@users.noreply.github.com>
2025-10-20 21:36:55 +0300

    fix: advertise host for redpanda

--- apps/basket/src/lib/producer.ts
+++ apps/basket/src/lib/producer.ts
@@ -1,45 +1,43 @@
-import { Kafka } from 'kafkajs';
+import { CompressionTypes, Kafka } from 'kafkajs';
 
-const BROKER = process.env.KAFKA_BROKERS || 'localhost:19092';
+const BROKER = process.env.KAFKA_BROKERS as string;
+console.log('BROKER', BROKER);
 
 const kafka = new Kafka({
   clientId: 'basket',
   brokers: [BROKER],
 });
 
-const producer = kafka.producer();
+const producer = kafka.producer({
+  allowAutoTopicCreation: true,
+});
 
 let connected = false;
+await producer.connect();
+connected = true;
 
-export const sendMessageSync = async (topic: string, message: string) => {
+export const sendEventSync = async (topic: string, event: any) => {
   try {
-    if (!connected) {
-      await producer.connect();
-      connected = true;
-    }
     await producer.send({
       topic,
-      messages: [{ value: message }],
+      messages: [{ value: event }],
+      compression: CompressionTypes.ZSTD,
     });
   } catch (err) {
-    console.error('Failed to send message', err);
+    console.error('Failed to send event', err);
     throw err;
   }
 };
 
 export const sendEvent = async (topic: string, event: any, key?: string) => {
   try {
-    if (!connected) {
-      await producer.connect();
-      connected = true;
-    }
-    
     producer.send({
       topic,
       messages: [{ 
         value: JSON.stringify(event),
         key: key || event.client_id
       }],
+      compression: CompressionTypes.ZSTD,
     });
   } catch (err) {
     console.error('Failed to send event', err);
