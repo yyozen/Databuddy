428f2b5c60c78b4d71daccc0614a1a8b04795fec
iza <59828082+izadoesdev@users.noreply.github.com>
2025-12-27 13:37:45 +0200

    fix; missing svelte code

--- apps/docs/components/docs/code-block.tsx
+++ apps/docs/components/docs/code-block.tsx
@@ -9,6 +9,7 @@ import json from "shiki/langs/json.mjs";
 import jsx from "shiki/langs/jsx.mjs";
 import markdown from "shiki/langs/markdown.mjs";
 import php from "shiki/langs/php.mjs";
+import svelte from "shiki/langs/svelte.mjs";
 import tsx from "shiki/langs/tsx.mjs";
 import githubLight from "shiki/themes/github-light.mjs";
 import vesper from "shiki/themes/vesper.mjs";
@@ -25,7 +26,7 @@ interface CodeBlockProps extends React.ComponentProps<"div"> {
 
 const highlighter = createHighlighterCoreSync({
 	themes: [vesper, githubLight],
-	langs: [tsx, jsx, html, css, json, markdown, bash, http, php],
+	langs: [tsx, jsx, html, css, json, markdown, bash, http, php, svelte],
 	engine: createJavaScriptRegexEngine(),
 });
 
