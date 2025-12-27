91a8b49bb6f928d39c40c8f41d0a25a61d1db448
iza <59828082+izadoesdev@users.noreply.github.com>
2025-12-27 17:08:03 +0200

    cleanup credits, no longer use ipdb

--- apps/basket/src/utils/ip-geo.ts
+++ apps/basket/src/utils/ip-geo.ts
@@ -12,8 +12,6 @@ interface GeoIPReader extends Reader {
 	city(ip: string): City;
 }
 
-// Geolocation database from DB-IP, db-ip.com
-// const CDN_URL = "https://cdn.databuddy.cc/mmdb/dbip-city-lite-2025-12.mmdb.gz";
 const CDN_URL = "https://cdn.databuddy.cc/mmdb/GeoLite2-City.mmdb";
 
 let reader: GeoIPReader | null = null;
