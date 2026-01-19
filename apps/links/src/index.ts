50b0c8fd49c996ef77e412f29f03a7c8a542c69f
iza <59828082+izadoesdev@users.noreply.github.com>
2026-01-19 19:34:55 +0200

    OTEL to links

--- apps/links/src/index.ts
+++ apps/links/src/index.ts
@@ -1,9 +1,28 @@
+import { opentelemetry } from "@elysiajs/opentelemetry";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-node";
 import { Elysia } from "elysia";
 import { disconnectProducer } from "./lib/producer";
 import { expiredRoute } from "./routes/expired";
 import { redirectRoute } from "./routes/redirect";
 
+const exporter = new OTLPTraceExporter({
+	url: "https://api.axiom.co/v1/traces",
+	headers: {
+		Authorization: `Bearer ${process.env.AXIOM_TOKEN}`,
+		"X-Axiom-Dataset": process.env.AXIOM_DATASET ?? "links",
+	},
+});
+
+const batchSpanProcessor = new BatchSpanProcessor(exporter);
+
 const app = new Elysia()
+	.use(
+		opentelemetry({
+			spanProcessor: batchSpanProcessor,
+			serviceName: "links",
+		})
+	)
 	.get("/", () => ({ status: "ok" }))
 	.get("/health", () => ({ status: "ok" }))
 	.use(expiredRoute)
