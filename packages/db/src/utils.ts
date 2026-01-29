b312aedddf3076568e8039f6e54d3af9fe04de5e
iza <59828082+izadoesdev@users.noreply.github.com>
2026-01-29 11:01:28 +0300

    fix: utils

--- packages/db/src/utils.ts
+++ packages/db/src/utils.ts
@@ -1,15 +1,17 @@
-import { isNull } from "drizzle-orm";
+import { isNull, type SQLWrapper } from "drizzle-orm";
 import type { PgTable } from "drizzle-orm/pg-core";
 import { db } from "./client";
 
 export function notDeleted<T extends PgTable>(
-	table: T & { deletedAt: unknown }
+	table: T & { deletedAt: SQLWrapper }
 ) {
 	return isNull(table.deletedAt);
 }
 
+type TransactionCallback = Parameters<typeof db.transaction>[0];
+
 export function withTransaction<T>(
-	callback: (tx: typeof db) => Promise<T>
+	callback: (tx: Parameters<TransactionCallback>[0]) => Promise<T>
 ): Promise<T> {
 	return db.transaction(callback);
 }
